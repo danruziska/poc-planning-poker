a9b1ba6b2a4bb2fa70a67efb1d6a9996869fe998
INTRA\danilo.ruziska <danilo.ruziska@uux.com>
2017-02-16 13:27:27 -0200

    Aplicação dos estilos no botão

--- src/components/action-choice.jsx
+++ src/components/action-choice.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import { defaultButton } from '../styles/ui-components/button';
-import { alternateButton } from '../styles/ui-components/button';
 
 export default class ActionChoice extends React.PureComponent{
-    
+
     render(){
-        var buttonStyle = {
-            backgroundColor: defaultButton.backgroundColor,          
+        var containerStyle = {
+            margin: 30,
+            textAlign: "center"  
         };
-
-        var labelStyle = {
-            color: defaultButton.color
-        }
-
-        var alternateButtonStyle = {
-            backgroundColor: alternateButton.backgroundColor,          
-        };
-
-        var alternateLabelStyle = {
-            color: alternateButton.color
-        }
-
         return(
-            <div>
-                <RaisedButton label="New Table" buttonStyle={buttonStyle} labelStyle={labelStyle} />
-                <RaisedButton label="Join Table" buttonStyle={alternateButtonStyle} labelStyle={alternateLabelStyle} />  
+            <div style={containerStyle}>
+                <RaisedButton label="New Table" buttonStyle={{backgroundColor:defaultButton.backgroundColor}} labelStyle={{color:defaultButton.color}} />
+                <RaisedButton label="Join Table" buttonStyle={{backgroundColor:defaultButton.backgroundColor}} labelStyle={{color:defaultButton.color}} />
             </div>              
         );
     }
