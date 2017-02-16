559bdf525b75cf21e06749428284c5bac5e421d6
INTRA\danilo.ruziska <danilo.ruziska@uux.com>
2017-02-16 17:29:07 -0200

    criação do layout grid e inserção do campo texto

--- src/components/action-choice.jsx
+++ src/components/action-choice.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import { defaultButton } from '../styles/ui-components/button';
+import TextField from 'material-ui/TextField';
+
+const {Grid, Row, Col} = require('react-flexbox-grid');
+
 
 export default class ActionChoice extends React.PureComponent{
 
     render(){
-        var containerStyle = {
-            margin: 30,
-            textAlign: "center"  
-        };
         return(
-            <div style={containerStyle}>
-                <RaisedButton label="New Table" buttonStyle={{backgroundColor:defaultButton.backgroundColor}} labelStyle={{color:defaultButton.color}} />
-                <RaisedButton label="Join Table" buttonStyle={{backgroundColor:defaultButton.backgroundColor}} labelStyle={{color:defaultButton.color}} />
-            </div>              
+            <Grid>
+                <Row style={{marginTop:'40px'}}>
+                    <Col style={{textAlign:'center'}}>
+                        <RaisedButton label="New Table" buttonStyle={{backgroundColor:defaultButton.backgroundColor}} labelStyle={{color:defaultButton.color}} />                                               
+                    </Col>                    
+                </Row>
+                <Row style={{margin:'10px'}}>                  
+                    <Col style={{textAlign:'center'}}>     
+                        <TextField floatingLabelFocusStyle={{color:'red'}}
+                            underlineFocusStyle={{borderBottomColor :'red'}}
+                            floatingLabelText="Code"
+                        />                   
+                        <RaisedButton label="Join Table" style={{marginLeft:'10px'}} buttonStyle={{backgroundColor:defaultButton.backgroundColor}} labelStyle={{color:defaultButton.color}} />
+                    </Col>                                        
+                </Row>                                
+            </Grid>              
         );
     }
 };
