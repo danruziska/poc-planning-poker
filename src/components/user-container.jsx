5982db9c809844cb012caa35f7aa8d6a5fb3644a
danruziska <ruziska.danilo@gmail.com>
2017-02-19 23:30:07 -0300

    estilização das cartas

--- src/components/user-container.jsx
+++ src/components/user-container.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 import User from './user';
 import RaisedButton from 'material-ui/RaisedButton';
 import { defaultButton } from '../styles/ui-components/button';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
 const io = require('socket.io-client');
 const socket = io('http://localhost:3001');
+
+const styles = {
+  customWidth: {
+    width: 150,
+  },
+};
 
 export default class UserContainer extends React.PureComponent{
 
     constructor() {
         super();
         this.state = {
-            cardValue:'',
+            cardValue:'1',
             user:{
                 id:'',
                 name:''
@@ -37,25 +46,30 @@ export default class UserContainer extends React.PureComponent{
     }
 
     sendCard(){
-        // var uuid = uuidV4();
-        // this.setState({
-        //     user:{
-        //         id: uuid,
-        //         name:this.state.user.name
-        //     }
-        // }, function(){
-        //     console.log('user id: ' + this.state.user.id);
-        //     socket.emit('join-room', this.state.user,this.state.roomIdField);
-        //     browserHistory.push('/user/' + this.state.user.name);
-        // });
         socket.emit('send-card',this.state.user,this.state.roomId, this.state.cardValue);
     }
+
+    handleChange = (event, index, value) => this.setState({cardValue:value});
 
     render(){ 
         return(  
             <div>
                 <User cardValue={this.state.cardValue} userName={this.props.params.userName} />
-                <input type="text" onChange={this.handlecardValueChange} />
+                <SelectField
+                    floatingLabelText="Card Value"
+                    value={this.state.cardValue}
+                    onChange={this.handleChange}
+                    style={styles.customWidth}
+                >
+                    <MenuItem value={1} primaryText="1" />
+                    <MenuItem value={2} primaryText="2" />
+                    <MenuItem value={3} primaryText="3" />
+                    <MenuItem value={5} primaryText="5" />
+                    <MenuItem value={8} primaryText="8" />
+                    <MenuItem value={13} primaryText="13" />
+                    <MenuItem value={20} primaryText="20" />
+                    <MenuItem value={40} primaryText="40" />
+                </SelectField>
                 <RaisedButton label="Send" 
                     buttonStyle={{backgroundColor:defaultButton.backgroundColor}} 
                     labelStyle={{color:defaultButton.color}} 
--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
+const cardStyle = {
+    position:'relative',
+    fontFamily:'Oswald',
+    fontSize:'10em',
+    borderStyle:'solid',
+    borderRadius:'10px',
+    textAlign:'center',
+    height:'300px',
+    width:'150px'
+}
+
 export default class User extends React.PureComponent{
     render(){ 
 
         return(  
             <div style={{margin:'20px', height:'300px', width:'150px'}}>
-                <div style={{height:'200px', width:'150px', borderStyle:'solid'}}>
-                    {this.props.cardValue}
+                <div style={cardStyle}>
+                    <span>{this.props.cardValue}</span>
                 </div>
                 <p style={{textAlign:'center'}}>{this.props.userName}</p>
             </div>                                                       
